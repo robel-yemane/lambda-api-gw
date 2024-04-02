39b10589e660657b471b092f4998be1c2c9cf4db
Robel Yemane <git@robel.slmail.me>
2024-04-02 14:07:48 +0100

    fix: remove buggy line (#3)
    * remove buggy line

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "lambda_bucket" {
 }
 
 resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
-  bucket = aws_s3_bucket.lambda_bucket.bucket.id
+  bucket = aws_s3_bucket.lambda_bucket.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "lambda_bucket" {
-  depends_on = [aws_s3_bucket.aws_s3_bucket_ownership_controls.lambda_bucket]
+  depends_on = [aws_s3_bucket_ownership_controls.lambda_bucket]
   bucket = aws_s3_bucket.lambda_bucket.id
   acl    = "private"
 }
