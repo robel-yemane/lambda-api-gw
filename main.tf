29b4612e80ca77f041d22f5a7a8a63a41b7a7807
robel yemane <git@robel.slmail.me>
2024-04-02 22:10:45 +0100

    create s3 bucket and upload lambda func

--- main.tf
+++ main.tf
@@ -6,10 +6,12 @@ resource "random_pet" "lambda_bucket_name" {
   length = 4
 }
 
+# Create an S3 bucket for the Lambda function
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = random_pet.lambda_bucket_name.id 
 }
 
+# Enable bucket ownership controls
 resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
   bucket = aws_s3_bucket.lambda_bucket.id
   rule {
@@ -18,13 +20,30 @@ resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
 
 }
 
+# Set the bucket ACL to private
 resource "aws_s3_bucket_acl" "lambda_bucket" {
   depends_on = [aws_s3_bucket_ownership_controls.lambda_bucket]
   bucket = aws_s3_bucket.lambda_bucket.id
   acl    = "private"
 }
   
+# package and copy hello-world func to the s3 bucket
+data "archive_file" "lambda_hello_world" {
+  type = "zip"
+  source_dir = "${path.module}/hello-world"
+  output_path = "${path.module}/hello-world.zip"
+}
 
+resource "aws_s3_object" "lambda_hello_world" {
+  bucket = aws_s3_bucket.lambda_bucket.id 
+
+  key = "hello-world.zip"
+  source = data.archive_file.lambda_hello_world.output_path
+  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+}
+
+
+####################################################################
 # #Lambda Func
 # resource "aws_lambda_function" "get_started_lambda" {
 #   function_name = "GetStartedLambdaFunc"
